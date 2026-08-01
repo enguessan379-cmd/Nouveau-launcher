24e4c8d0f7e2b56552186bf31bb816f42001937c
TonNom <ton@email.com>
2026-08-01 20:49:27 +0000

    Fix: IP/port du serveur codes en dur, ignoraient le serveur choisi dans le menu

--- app/src/main/cpp/samp/main.cpp
+++ app/src/main/cpp/samp/main.cpp
@@ -254,7 +254,7 @@ void DoInitStuff()
 	{
 		//ReadSettingFile();
 
-		pNetGame = new CNetGame("94.23.168.153", 2305, pSettings->Get().szNickName, pSettings->Get().szPassword);
+		pNetGame = new CNetGame(pSettings->Get().szHost, pSettings->Get().iPort, pSettings->Get().szNickName, pSettings->Get().szPassword);
 		bNetworkInited = true;
 
         FLog("DoInitStuff end");
